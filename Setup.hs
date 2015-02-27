bcfab213ad74869c6f9c4bd4826378d6d4627082
Slava Bacherikov <slava@bacher09.org>
2015-02-27 14:06:17 +0200

    Change lexer mode to latin1

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,19 @@
 import Distribution.Simple
-main = defaultMain
+import Distribution.Simple.Setup
+import Data.List (elem)
+
+
+main = defaultMainWithHooks myhooks
+  where
+    hooks = simpleUserHooks
+    myconf = wrapConfHook $ confHook hooks
+    myhooks = hooks {confHook=myconf}
+
+
+wrapConfHook f args conf = f args conf'
+  where
+    pargs = configProgramArgs conf
+    alex_args = ("alex", ["--latin1"])
+    conf' = if alex_args `elem` pargs
+        then conf
+        else conf {configProgramArgs=alex_args : pargs}
