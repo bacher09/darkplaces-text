53547f6642311f403f9088caa84d7bde484ccb94
Slava Bacherikov <slava@bacher09.org>
2015-06-21 17:08:51 +0300

    Remove alex based lexer

--- Setup.hs
+++ Setup.hs
@@ -1,19 +1,2 @@
 import Distribution.Simple
-import Distribution.Simple.Setup
-import Data.List (elem)
-
-
-main = defaultMainWithHooks myhooks
-  where
-    hooks = simpleUserHooks
-    myconf = wrapConfHook $ confHook hooks
-    myhooks = hooks {confHook=myconf}
-
-
-wrapConfHook f args conf = f args conf'
-  where
-    pargs = configProgramArgs conf
-    alex_args = ("alex", ["--latin1"])
-    conf' = if alex_args `elem` pargs
-        then conf
-        else conf {configProgramArgs=alex_args : pargs}
+main = defaultMain
